6d66018db12fb852c7402c60372e498c8845c0f8
Eduardo Ulibarri Toledo <eduulitol@gmail.com>
2019-10-09 18:46:09 +0200

    exercise 7

--- Python Exercises/Exercise 6/calculate_marks.py
+++ Python Exercises/Exercise 6/calculate_marks.py
@@ -4,18 +4,20 @@ def calculate_marks(solutions, students_answers):
     #   Iterate from student to student
     for student in students_answers:
         mark = 0
-        print("Student: {0}".format(student))
+        print("Student ID: {0} ------------".format(student))
+        print("Student {0} answers: {1}".format(student, students_answers[student]))
         #   Iterate through student answers
         for answer_key in students_answers[student]:
-            print("Student {0} answers: {1}".format(student, students_answers[student]))
             print("Student answer to question {0}: {1}".format(answer_key, students_answers[student][answer_key]))
             if not (answer_key in id_questions):
                 print(
                     "Question ID {0} is not in the answer sheet, valid IDs are: {1}".format(answer_key, id_questions))
                 pass
             elif students_answers[student][answer_key] == solutions[answer_key]:
+                print ("Answer to question {0} is {1}. +1".format(answer_key, students_answers[student][answer_key]))
                 mark += 1
             else:
+                print ("Answer to question {0} is {1}. -0.25".format(answer_key, students_answers[student][answer_key]))
                 mark -= 0.25
 
         ret[student] = mark
---
+++ Python Exercises/Exercise 7/figure.py
@@ -0,0 +1,51 @@
+import math
+
+
+class Figure:
+    """Template figure class"""
+    def area(self):
+        pass
+
+    @staticmethod
+    def areas (figures):
+        """Get a list with the areas of a list of figures"""
+        areas = []
+        for figure in figures:
+            areas.append(figure.area())
+        return areas
+
+    @staticmethod
+    def are_archenemy (figure1, figure2):
+        """Say if figure 1 and figure2 are figures and have the same type"""
+        return isinstance(figure1, Figure) and isinstance(figure2, Figure) and figure1.__class__ == figure2.__class__
+
+
+class Square (Figure):
+    def __init__(self, side_length):
+        super().__init__()
+        self.width = side_length
+        self.height = side_length
+
+    def area(self):
+        return self.width*self.height
+
+
+class Circle (Figure):
+    def __init__(self, radius):
+        super().__init__()
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius**2
+
+
+c1 = Circle(2)
+c2 = Circle(3)
+s1 = Square(2)
+s2 = Square(3)
+
+print (c1.area())
+print (c2.area())
+print (s1.area())
+print (s2.area())
+print(Figure.are_archenemy(s1, c2))
