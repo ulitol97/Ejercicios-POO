4a7e5fa4e31c13b92dfff6d675b9249f86857c7e
Eduardo Ulibarri Toledo <eduulitol@gmail.com>
2019-10-14 13:41:36 +0200

    exercise 5 completed

--- Python Exercises/Exercise5/team.py
+++ Python Exercises/Exercise5/team.py
@@ -1,39 +1,53 @@
 class Person:
+    """Parent class for players and coaches, who have a name attribute"""
     def __init__(self, name):
         self.name = name
 
 
 class Player (Person):
-
+    """Represents a player with a name and a number"""
     def __init__(self, name, number):
         super().__init__(name)
         self.number = number
 
+    def __str__(self):
+        return 'Player: {0} - Number: {1}'.format(self.name, self.number)
+
 
 class Coach(Person):
-
+    """Represents a coach with a name"""
     def __init__(self, name):
         super().__init__(name)
 
 
 class Team:
+    """Represents a Team with a list of players"""
+    def __init__(self, players=None, coach=None):
+        self.players = players
+        self.coach = coach
 
-    def __init__(self):
-        self.players = []
+    def __str__(self):
+        ret = 'Coach:\n\t{0}\nPlayers:\n'.format(self.coach.name)
+        for player in self.players:
+            ret += '\t'+player.__str__()+'\n'
+        return ret
 
 
 if __name__ == "__main__":
-    my_team = Team()
-    adcarry = Player ("Carry", 1)
-    support = Player ("Support", 2)
-    jungler = Player ("Jungle", 3)
-    top = Player ("Top", 4)
-    mid = Player ("Mid", 5)
-    my_team.players.append(adcarry)
-    my_team.players.append(support)
-    my_team.players.append(jungler)
-    my_team.players.append(top)
-    my_team.players.append(mid)
+    """Sample main to test functionality. Create a list of players and a coach.
+    Init a team and print its members"""
 
-    for i in range(len(my_team.players)):
-        print("Position: {0} - Number: {1}".format(my_team.players[i].name, my_team.players[i].number))
+    # Create a list of players simulating a LoL team
+    adcarry = Player("Carry", 1)
+    support = Player("Support", 2)
+    jungler = Player("Jungle", 3)
+    top = Player("Top", 4)
+    mid = Player("Mid", 5)
+
+    players = [top, jungler, mid, adcarry, support]
+    # Create a coach
+    coach = Coach('Xpeke')
+
+    # Create a team given the list of players and a coach
+    my_team = Team(players, coach)
+    print(my_team)
