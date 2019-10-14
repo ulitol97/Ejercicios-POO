f3e67e2a9550fe88b7ee94aebdb0b29dc8055b2d
Eduardo Ulibarri Toledo <eduulitol@gmail.com>
2019-10-14 14:22:39 +0200

    exercise 6 ready

--- Python Exercises/Exercise6/calculate_marks.py
+++ Python Exercises/Exercise6/calculate_marks.py
@@ -4,7 +4,7 @@ def calculate_marks(solutions, students_answers):
     #   Iterate from student to student
     for student in students_answers:
         mark = 0
-        print("Student ID: {0} ------------".format(student))
+        print("\nStudent ID: {0} ------------".format(student))
         print("Student {0} answers: {1}".format(student, students_answers[student]))
         #   Iterate through student answers
         for answer_key in students_answers[student]:
@@ -17,13 +17,15 @@ def calculate_marks(solutions, students_answers):
                 print ("Answer to question {0} is {1}. +1".format(answer_key, students_answers[student][answer_key]))
                 mark += 1
             else:
-                print ("Answer to question {0} is {1}. -0.25".format(answer_key, students_answers[student][answer_key]))
+                print ("Answer to question {0} is {1}. -0.25".format(answer_key, solutions[answer_key]))
                 mark -= 0.25
 
+        print ('\nSTUDENTS\' MARKS')
         ret[student] = mark
 
     return ret
 
 
 if __name__ == "__main__":
-    print(calculate_marks({1: 'a', 2: 'b'}, {1: {1: 'a', 2: 'c'}}))
+    """Sample main to test functionality. Compute the marks of a couple of students"""
+    print(calculate_marks({1: 'a', 2: 'b'}, {1: {1: 'a', 2: 'c'}, 2: {1: 'b', 2: 'b'}}))
