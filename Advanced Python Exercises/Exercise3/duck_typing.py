79bf19246b3a903ce9f84a93c7c04e97766cbbfc
Eduardo Ulibarri Toledo <eduulitol@gmail.com>
2019-10-14 17:44:30 +0200

    advanced exercise 3 to be fixed

--- Advanced Python Exercises/Exercise3/duck_typing.py
+++ Advanced Python Exercises/Exercise3/duck_typing.py
@@ -1,9 +1,9 @@
-# El treeset es un set y por tanto ordenado. Al meterle una persona debe saber como compararlas.
+# El treeset es un set y que al meterle una persona debe saber como compararlas.
 # Necesitamos implementar el Compare() en persona
 
 
 class Person:
-
+    """Sample person class"""
     def __init__(self, name, surname, age=18, dni='1234567V'):
         self.name = name
         self.surname = surname
@@ -18,7 +18,7 @@ class Person:
 
 
 class TreeSet:
-
+    """My TreeSet implementation"""
     def __init__(self, comparator=None):
         self.contents = []  # Implemented as an array for simplicity
         self.comparator = comparator
@@ -54,7 +54,7 @@ class TreeSet:
 
 
 class ComparatorDNI:
-
+    """A comparator by DNI for the TreeSet to use"""
     def compare(self, person1, person2):
         if person1.dni >= person2.dni:
             return 1
@@ -63,7 +63,7 @@ class ComparatorDNI:
 
 
 class ComparatorAge:
-
+    """A comparator by age for the TreeSet to use"""
     def compare(self, person1, person2):
         if person1.age >= person2.age:
             return 1
@@ -76,13 +76,25 @@ if __name__ == '__main__':
     tree_set_dni = TreeSet()  # Tree set comparing by DNI
     tree_set_age = TreeSet()  # Tree set comparing by age
 
-    p1 = Person("Edu", "A", 21)
-    p2 = Person("Alex", "B", 22)
-    p3 = Person("Marcial", "C", 23)
+    p1 = Person("Edu", "A", 70, "251436")
+    p2 = Person("Alex", "B", 28, "251427")
+    p3 = Person("Marcial", "C", 15, "251851")
 
     tree_set.add(p1)
     tree_set.add(p2)
     tree_set.add(p3)
 
+    tree_set_dni.add(p1)
+    tree_set_dni.add(p2)
+    tree_set_dni.add(p3)
+
+    tree_set_age.add(p1)
+    tree_set_age.add(p2)
+    tree_set_age.add(p3)
+
+    print("\nComparison by Surname-Name")
     print(tree_set)
-    print(type(TreeSet))
+    print("\nComparison by Age")
+    print(tree_set_age)
+    print("\nComparison by DNI")
+    print(tree_set_dni)
