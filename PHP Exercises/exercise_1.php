96e571d9f932aa14dc36c348def40f5455524e60
Eduardo Ulibarri Toledo <eduulitol@gmail.com>
2019-10-18 13:26:21 +0200

    php exercise 1 ready

--- PHP Exercises/exercise_1.php
+++ PHP Exercises/exercise_1.php
@@ -1,5 +1,6 @@
 <?php
 
+// Coordinate to represent 2D points
 class Coordinate {
   private $x;
   private $y;
@@ -12,6 +13,8 @@ class Coordinate {
       return "x: " . $this->x . " - y: " . $this->y;
   }
 }
+
+// Template Figure class
 class Figure {
   private $center; // Center location 
   public $n_vertices; // Number of vertices of the figure
@@ -26,18 +29,24 @@ class Figure {
     }
   }
 
+  // String representation of the Figure
   public function __toString() {
-    $ret =  "Center:\n\t" . $this->center->__toString() . "\nVertices:\n";
-    for ($i = 0; $i < $this->n_vertices; $i++){
-      $ret = $ret . "\t" . $this->vertices[$i]->__toString() . "\n";
+    $ret =  "Center:\n\t" . $this->center->__toString();
+    if (count($this->vertices) > 0){
+      $ret = $ret . "\nVertices:\n";
+
+      for ($i = 0; $i < $this->n_vertices; $i++){
+        $ret = $ret . "\t" . $this->vertices[$i]->__toString() . "\n";
+      }
     }
     return $ret;
   }
 }
 
+// Triangle figure
 class Triangulo extends Figure {
   function __construct($center, $vertices){
-      parent::__construct($center, $vertices, 3);
+      parent::__construct($center, $vertices, 3); // Maximum of 3 vertices
     }
   
   public function __toString() {
@@ -45,22 +54,37 @@ class Triangulo extends Figure {
   }
 }
 
+// Square figure
 class Cuadrado extends Figure {
   function __construct($center, $vertices){
-    parent::__construct($center, $vertices, 4);
+    parent::__construct($center, $vertices, 4); // Maximum of 4 vertices
   }
   public function __toString() {
     return "\nCUADRADO\n" . parent::__toString();
   }
 }
 
+// Poligon figure
 class Poligono extends Figure {
   function __construct($center, $vertices){
-    parent::__construct($center, $vertices, count($vertices));
+    parent::__construct($center, $vertices, count($vertices)); // As many vertices as the length of the array passed
   }
 
   public function __toString() {
     return "\nPOLIGONO (lados: " . $this->n_vertices . ")\n" . parent::__toString();
+  }
+}
+
+// Circle figure: the circle has a radius and no vertices specified to the parent constructor
+class Circulo extends Figure {
+  private $radius;
+  function __construct($center, $radius){
+    $this->radius = $radius;
+    parent::__construct($center, array(), 0);
+  }
+
+  public function __toString() {
+    return "\nCirculo (radio: " . $this->radius . ")\n" . parent::__toString();
   }
 }
 
@@ -74,7 +98,7 @@ class Poligono extends Figure {
 // Tipos al ejercicio 1.
 
 
-// Program
+// Program. Trying to build a figure with less sides that it needs throws an exception.
 $coords = array();
 array_push ($coords, new Coordinate(1,1));
 array_push ($coords, new Coordinate(2,2));
@@ -82,7 +106,6 @@ array_push ($coords, new Coordinate(3,3));
 array_push ($coords, new Coordinate(4,4));
 array_push ($coords, new Coordinate(5,5));
 array_push ($coords, new Coordinate(6,6));
-
 
 $tri = new Triangulo(new Coordinate(4,4), $coords);
 echo ($tri);
@@ -92,4 +115,7 @@ echo ($cuad);
 
 $poli = new Poligono(new Coordinate(4,4), $coords);
 echo ($poli);
+
+$circle = new Circulo(new Coordinate(4,4), 5);
+echo ($circle);
 ?>
